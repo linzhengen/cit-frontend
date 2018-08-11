1b64460220cc196d9b22bb296a048ca8a9f03c24
linzhengen <linzhengen@yahoo.co.jp>
2018-08-12 01:10:06 +0900

    update viewport

--- src/views/report/Form.vue
+++ src/views/report/Form.vue
@@ -6,401 +6,52 @@
       <hr>
       <div class="columns">
         <div class="column form-column">
-          <div class="field">
-            <label class="label">Name</label>
-            <p class="control">
-              <input
-                class="input"
+          <div
+            v-for="item in form_items"
+            :key="item.item_code"
+            class="field">
+            <label class="label">{{ item.name }}</label>
+            <p
+              v-if="item.element_type === 'text'"
+              class="control">
+              <el-input
+                v-model="form[item.item_code]"
+                :placeholder="item.name"
                 type="text"
-                placeholder="Text input">
+                class="form-item"/>
             </p>
-          </div>
-          <div class="field">
-            <label class="label">Username</label>
-            <p class="control has-icons-left has-icons-right">
-              <input
-                class="input is-success"
-                type="text"
-                placeholder="Text input"
-                value="bulma">
-              <span class="icon is-small is-left">
-                <i class="fa fa-user"/>
-              </span>
-              <span class="icon is-small is-right">
-                <i class="fa fa-check"/>
-              </span>
+            <p
+              v-else-if="item.element_type === 'datetime'"
+              class="control">
+              <el-date-picker
+                v-model="form[item.item_code]"
+                :placeholder="item.name"
+                type="datetime"
+                class="form-item"/>
             </p>
-            <p class="help is-success">This username is available</p>
-          </div>
-          <div class="field">
-            <label class="label">Email</label>
-            <p class="control has-icons-left has-icons-right">
-              <input
-                class="input is-danger"
-                type="text"
-                placeholder="Email input"
-                value="hello@">
-              <span class="icon is-small is-left">
-                <i class="fa fa-envelope"/>
-              </span>
-              <span class="icon is-small is-right">
-                <i class="fa fa-warning"/>
-              </span>
+            <p
+              v-else-if="item.element_type === 'number'"
+              class="control">
+              <el-input-number
+                v-model="form[item.item_code]"
+                :min="0"
+                :placeholder="item.name"
+                class="form-item"/>
             </p>
-            <p class="help is-danger">This email is invalid</p>
-          </div>
-          <div class="field">
-            <label class="label">Subject</label>
-            <p class="control">
-              <span class="select">
-                <select>
-                  <option>Select dropdown</option>
-                  <option>With options</option>
-                </select>
-              </span>
+            <p
+              v-else-if="item.element_type === 'select'"
+              class="control">
+              <el-select
+                v-model="form[item.item_code]"
+                :placeholder="item.name"
+                class="form-item">
+                <el-option
+                  v-for="option in item.options"
+                  :key="option.type_id"
+                  :label="option.name"
+                  :value="option.type_id"/>
+              </el-select>
             </p>
-          </div>
-          <div class="field">
-            <label class="label">Message</label>
-            <p class="control">
-              <textarea
-                class="textarea"
-                placeholder="Textarea"/>
-            </p>
-          </div>
-          <div class="field">
-            <p class="control">
-              <label class="checkbox">
-                <input type="checkbox"> I agree to the
-                <a href="#">terms and conditions</a>
-              </label>
-            </p>
-          </div>
-          <div class="field">
-            <p class="control">
-              <label class="radio">
-                <input
-                  type="radio"
-                  name="question"> Yes
-              </label>
-              <label class="radio">
-                <input
-                  type="radio"
-                  name="question"> No
-              </label>
-            </p>
-          </div>
-          <div class="field is-grouped">
-            <p class="control">
-              <button class="button is-primary">Submit</button>
-            </p>
-            <p class="control">
-              <button class="button is-link">Cancel</button>
-            </p>
-          </div>
-          <br>
-          <h4 class="subtitle"> Disabled </h4>
-          <hr>
-          <div class="field">
-            <p class="control">
-              <input
-                class="input"
-                disabled=""
-                placeholder="Disabled input"
-                type="text">
-            </p>
-          </div>
-          <div class="field">
-            <p class="control">
-              <textarea
-                class="textarea"
-                disabled=""
-                placeholder="Disabled textarea"/>
-            </p>
-          </div>
-          <div class="field">
-            <p class="control">
-              <label class="checkbox is-disabled">
-                <input
-                  disabled=""
-                  type="checkbox"> Remember me
-              </label>
-            </p>
-          </div>
-          <div class="field">
-            <p class="control">
-              <label class="radio is-disabled">
-                <input
-                  disabled=""
-                  name="question"
-                  type="radio"> Yes
-              </label>
-              <label class="radio is-disabled">
-                <input
-                  disabled=""
-                  name="question"
-                  type="radio"> No
-              </label>
-            </p>
-          </div>
-          <div class="field is-grouped">
-            <p class="control">
-              <button
-                class="button is-primary"
-                disabled=""> Submit
-              </button>
-            </p>
-            <p class="control">
-              <button
-                class="button"
-                disabled=""> Cancel
-              </button>
-            </p>
-          </div>
-          <br>
-          <h3 class="title"> Horizontal Form </h3>
-          <hr>
-          <div class="field is-horizontal">
-            <div class="field-label is-normal">
-              <label class="label">From</label>
-            </div>
-            <div class="field-body">
-              <div class="field is-grouped">
-                <p class="control is-expanded has-icons-left">
-                  <input
-                    class="input"
-                    type="text"
-                    placeholder="Name">
-                  <span class="icon is-small is-left">
-                    <i class="fa fa-user"/>
-                  </span>
-                </p>
-              </div>
-              <div class="field">
-                <p class="control is-expanded has-icons-left has-icons-right">
-                  <input
-                    class="input is-success"
-                    type="email"
-                    placeholder="Email"
-                    value="alex@smith.com">
-                  <span class="icon is-small is-left">
-                    <i class="fa fa-envelope"/>
-                  </span>
-                  <span class="icon is-small is-right">
-                    <i class="fa fa-check"/>
-                  </span>
-                </p>
-                <p class="help is-success">This email is correct</p>
-              </div>
-            </div>
-          </div>
-          <div class="field is-horizontal">
-            <div class="field-label is-normal">
-              <label class="label">Department</label>
-            </div>
-            <div class="field-body">
-              <div class="field is-narrow">
-                <div class="control">
-                  <div class="select is-fullwidth">
-                    <select>
-                      <option>Business development</option>
-                      <option>Marketing</option>
-                      <option>Sales</option>
-                    </select>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="field is-horizontal">
-            <div class="field-label">
-              <label class="label">Already a member?</label>
-            </div>
-            <div class="field-body">
-              <div class="field is-narrow">
-                <div class="control">
-                  <label class="radio">
-                    <input
-                      type="radio"
-                      name="member"> Yes
-                  </label>
-                  <label class="radio">
-                    <input
-                      type="radio"
-                      name="member"> No
-                  </label>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="field is-horizontal">
-            <div class="field-label is-normal">
-              <label class="label">Subject</label>
-            </div>
-            <div class="field-body">
-              <div class="field">
-                <div class="control">
-                  <input
-                    class="input is-danger"
-                    type="text"
-                    placeholder="e.g. Partnership opportunity">
-                </div>
-                <p class="help is-danger">
-                  This field is required
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="field is-horizontal">
-            <div class="field-label is-normal">
-              <label class="label">Question</label>
-            </div>
-            <div class="field-body">
-              <div class="field">
-                <div class="control">
-                  <textarea
-                    class="textarea"
-                    placeholder="Explain how we can help you"/>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="field is-horizontal">
-            <div class="field-label">
-              <!-- Left empty for spacing -->
-            </div>
-            <div class="field-body">
-              <div class="field">
-                <div class="control">
-                  <button class="button is-primary">
-                    Send message
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <br>
-          <h3 class="title">File</h3>
-          <hr>
-          <div class="field">
-            <div class="file">
-              <label class="file-label">
-                <input
-                  class="file-input"
-                  type="file"
-                  name="resume">
-                <span class="file-cta">
-                  <span class="file-icon">
-                    <i class="fa fa-upload"/>
-                  </span>
-                  <span class="file-label">
-                    Choose a file…
-                  </span>
-                </span>
-              </label>
-            </div>
-          </div>
-          <div class="field">
-            <div class="file has-name">
-              <label class="file-label">
-                <input
-                  class="file-input"
-                  type="file"
-                  name="resume">
-                <span class="file-cta">
-                  <span class="file-icon">
-                    <i class="fa fa-upload"/>
-                  </span>
-                  <span class="file-label">
-                    Choose a file…
-                  </span>
-                </span>
-                <span class="file-name">
-                  Screen Shot 2017-07-29 at 15.54.25.png
-                </span>
-              </label>
-            </div>
-          </div>
-          <div class="field">
-            <div class="file is-primary">
-              <label class="file-label">
-                <input
-                  class="file-input"
-                  type="file"
-                  name="resume">
-                <span class="file-cta">
-                  <span class="file-icon">
-                    <i class="fa fa-upload"/>
-                  </span>
-                  <span class="file-label">
-                    Primary file…
-                  </span>
-                </span>
-              </label>
-            </div>
-          </div>
-
-          <div class="field">
-            <div class="file is-info has-name">
-              <label class="file-label">
-                <input
-                  class="file-input"
-                  type="file"
-                  name="resume">
-                <span class="file-cta">
-                  <span class="file-icon">
-                    <i class="fa fa-upload"/>
-                  </span>
-                  <span class="file-label">
-                    Info file…
-                  </span>
-                </span>
-                <span class="file-name">
-                  Screen Shot 2017-07-29 at 15.54.25.png
-                </span>
-              </label>
-            </div>
-          </div>
-
-          <div class="field">
-            <div class="file is-warning is-boxed">
-              <label class="file-label">
-                <input
-                  class="file-input"
-                  type="file"
-                  name="resume">
-                <span class="file-cta">
-                  <span class="file-icon">
-                    <i class="fa fa-cloud-upload"/>
-                  </span>
-                  <span class="file-label">
-                    Warning file…
-                  </span>
-                </span>
-              </label>
-            </div>
-          </div>
-
-          <div class="field">
-            <div class="file is-danger has-name is-boxed">
-              <label class="file-label">
-                <input
-                  class="file-input"
-                  type="file"
-                  name="resume">
-                <span class="file-cta">
-                  <span class="file-icon">
-                    <i class="fa fa-cloud-upload"/>
-                  </span>
-                  <span class="file-label">
-                    Danger file…
-                  </span>
-                </span>
-                <span class="file-name">
-                  Screen Shot 2017-07-29 at 15.54.25.png
-                </span>
-              </label>
-            </div>
           </div>
         </div>
       </div>
@@ -410,12 +61,101 @@
 
 <script>
   export default {
-    name: 'ReportForm'
+    name: 'ReportForm',
+    data() {
+      return {
+        form: {},
+        form_items: [
+          {
+            element_type: 'select',
+            item_code: 'type',
+            name: '聚會類別',
+            required: true,
+            options: [
+              {type_id: 1, name: '小排'},
+              {type_id: 2, name: '一般聚會'},
+              {type_id: 3, name: '特別聚會'},
+              {type_id: 4, name: '真理成全'},
+            ]
+          },
+          {
+            element_type: 'text',
+            item_code: 'title',
+            name: '標題',
+            required: true,
+          },
+          {
+            element_type: 'text',
+            item_code: 'place',
+            name: '地點',
+            required: true,
+          },
+          {
+            element_type: 'datetime',
+            item_code: 'time',
+            name: '時間',
+            required: true,
+          },
+          {
+            element_type: 'number',
+            item_code: 'report_item_1',
+            name: '弟兄',
+            required: false,
+          },
+          {
+            element_type: 'number',
+            item_code: 'report_item_2',
+            name: '姊妹',
+            required: false,
+          },
+          {
+            element_type: 'number',
+            item_code: 'report_item_3',
+            name: '受浸',
+            required: false,
+          },
+          {
+            element_type: 'number',
+            item_code: 'report_item_4',
+            name: '新人',
+            required: false,
+          },
+          {
+            element_type: 'number',
+            item_code: 'report_item_5',
+            name: '朋友',
+            required: false,
+          },
+          {
+            element_type: 'number',
+            item_code: 'report_item_6',
+            name: '兒童',
+            required: false,
+          },
+          {
+            element_type: 'number',
+            item_code: 'report_item_summary',
+            name: '合計',
+            required: true,
+          },
+          {
+            element_type: 'number',
+            item_code: 'report_item_speak',
+            name: '申言',
+            required: false,
+          },
+        ]
+      }
+    },
   }
 </script>
 
 <style lang="scss" scoped>
   .form-column {
     max-width: 700px;
+  }
+  .form-item {
+    min-width: 300px;
+    max-width: 500px;
   }
 </style>
