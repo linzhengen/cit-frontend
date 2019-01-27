51758ae80a6ac6ca8928c72d576e2767d46a3333
linzhengen <linzhengen@yahoo.co.jp>
2019-01-28 03:14:08 +0900

    form item

--- src/views/report/Form.vue
+++ src/views/report/Form.vue
@@ -8,63 +8,70 @@
         <div class="column form-column">
           <div
             v-for="item in reportForm"
-            :key="item.item_code"
+            :key="item.name"
             class="field is-horizontal">
             <div class="field-label is-normal">
-              <label class="label">{{ item.name }}</label>
+              <label class="label">{{ item.label }}</label>
             </div>
             <div class="field-body">
               <div class="field is-narrow">
                 <div
-                  v-if="item.element_type === 'text'"
+                  v-if="item.type === 'text'"
                   class="control">
                   <el-input
-                    v-model="form[item.item_code]"
-                    :placeholder="item.name"
+                    v-model="form[item.name]"
+                    :placeholder="item.label"
                     type="text"
                     class="form-item"/>
                 </div>
                 <div
-                  v-else-if="item.element_type === 'datetime'"
+                  v-else-if="item.type === 'datetime'"
                   class="control">
                   <el-date-picker
-                    v-model="form[item.item_code]"
-                    :placeholder="item.name"
+                    v-model="form[item.name]"
+                    :placeholder="item.label"
                     :editable="false"
                     type="datetime"
                     class="form-item"/>
                 </div>
                 <div
-                  v-else-if="item.element_type === 'number'"
+                  v-else-if="item.type === 'number'"
                   class="control">
                   <el-input-number
-                    v-model="form[item.item_code]"
+                    v-model="form[item.name]"
                     :min="0"
-                    :placeholder="item.name"
-                    class="form-item"/>
+                    :placeholder="item.label"
+                    class="form-item"
+                    @change="summaryReport"/>
                 </div>
                 <div
-                  v-else-if="item.element_type === 'select'"
+                  v-else-if="item.type === 'select'"
                   class="control">
                   <el-select
-                    v-model="form[item.item_code]"
-                    :placeholder="item.name"
-                    class="form-item">
+                    v-model="form[item.name]"
+                    :placeholder="item.label"
+                    class="form-item"
+                    @change="selectChange(item.name)">
                     <el-option
                       v-for="option in item.options"
-                      :key="option.type_id"
-                      :label="option.name"
-                      :value="option.type_id"/>
+                      :key="option.key"
+                      :label="option.label"
+                      :value="option.key"/>
                   </el-select>
                 </div>
                 <div
-                  v-else-if="item.element_type === 'autocomplete'"
+                  v-else-if="item.type === 'placeAutoComplete'"
                   class="control">
                   <el-autocomplete
-                    v-model="form[item.item_code]"
+                    v-model="form[item.name]"
+                    :placeholder="item.label"
                     :fetch-suggestions="querySearch"
-                    placeholder="请输入内容"
-                    class="inline-input form-item"/>
+                    class="form-item"
+                    @select="handleAutocomplete">
+                    <template slot-scope="{ item }">
+                      <div class="name">{{ item.name }}</div>
+                    </template>
+                  </el-autocomplete>
                 </div>
               </div>
             </div>
@@ -78,15 +85,47 @@
 <script>
   import places from "@/data/places";
   import reportForm from "@/data/reportForm"
+
   export default {
     name: 'ReportForm',
     data() {
       return {
         form: {},
         places: places,
+        placeSelect: [],
         reportForm: reportForm,
       }
     },
+    methods: {
+      selectChange(name) {
+        if (name === 'type') {
+          this.placeSelect = this.places[this.form[name]];
+          delete this.form['place'];
+        }
+      },
+      querySearch(queryString, cb) {
+        let placeSelect = this.placeSelect;
+        let results = queryString ? placeSelect.filter(this.createFilter(queryString)) : placeSelect;
+        cb(results);
+      },
+      createFilter(queryString) {
+        return (placeSelect) => {
+          return (placeSelect.name.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
+        };
+      },
+      handleAutocomplete(item) {
+        this.form['place'] = item.name;
+      },
+      summaryReport() {
+        this.form['report_item_summary'] =
+            (this.form['report_item_1'] || 0)
+          + (this.form['report_item_2'] || 0)
+          + (this.form['report_item_3'] || 0)
+          + (this.form['report_item_4'] || 0)
+          + (this.form['report_item_5'] || 0)
+          + (this.form['report_item_6'] || 0);
+      },
+    }
   }
 </script>
 
@@ -94,6 +133,7 @@
   .form-column {
     max-width: 700px;
   }
+
   .form-item {
     min-width: 340px;
     max-width: 400px;
